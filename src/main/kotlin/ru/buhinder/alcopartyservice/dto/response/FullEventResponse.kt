fa8f1e60db78194ee553e15aaa313cdd496e7571
Alexander Kunakov <kf@kode.ru>
2022-08-31 21:46:44 +0300

    [BUH-T-28] Add event participants ids to response

--- src/main/kotlin/ru/buhinder/alcopartyservice/dto/response/FullEventResponse.kt
+++ src/main/kotlin/ru/buhinder/alcopartyservice/dto/response/FullEventResponse.kt
@@ -5,4 +5,5 @@ import java.util.UUID
 data class FullEventResponse(
     val event: EventResponse,
     val images: List<UUID>,
+    val participants: List<UUID>,
 )
--- src/main/kotlin/ru/buhinder/alcopartyservice/repository/facade/EventAlcoholicDaoFacade.kt
+++ src/main/kotlin/ru/buhinder/alcopartyservice/repository/facade/EventAlcoholicDaoFacade.kt
@@ -5,6 +5,7 @@ import org.springframework.data.relational.core.query.Criteria
 import org.springframework.data.relational.core.query.CriteriaDefinition
 import org.springframework.data.relational.core.query.Query
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import ru.buhinder.alcopartyservice.config.LoggerDelegate
 import ru.buhinder.alcopartyservice.entity.EventAlcoholicEntity
@@ -50,6 +51,10 @@ class EventAlcoholicDaoFacade(
             eventId = eventId,
             alcoholicId = alcoholicId
         )
+    }
+
+    fun findAllByEventId(eventId: UUID): Flux<EventAlcoholicEntity> {
+        return eventAlcoholicRepository.findAllByEventId(eventId)
     }
 
 }
